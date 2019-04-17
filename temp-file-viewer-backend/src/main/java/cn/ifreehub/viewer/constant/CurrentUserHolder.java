ab2b332603bf99ebc0d789917d991634ab47fc78
ddyang <1273275027@qq.com>
2019-04-17 15:58:26 +0800

    修复错误,关闭图片瘦身

--- temp-file-viewer-backend/src/main/java/cn/ifreehub/viewer/constant/CurrentUserHolder.java
+++ temp-file-viewer-backend/src/main/java/cn/ifreehub/viewer/constant/CurrentUserHolder.java
@@ -4,33 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CurrentUserHolder {
-    public static ThreadLocal<Map<String,String>> current = new ThreadLocal<Map<String,String>>();
+    public static ThreadLocal<Map<String, String>> current = new ThreadLocal<>();
 
     /**
      * 设置登录用户名
+     *
      * @param userName
      */
-    public static void setUserName(String userName){
+    public static void setUserName(String userName) {
         Map<String, String> map = current.get();
-        if (map == null){
+        if (map == null) {
             map = new HashMap<>();
-            map.put("userName",userName);
+            map.put("userName", userName);
             current.set(map);
-        }else {
-            if (map.get("userName") == null){
-                map.put("userName",userName);
-            }
+        } else {
+            map.put("userName", userName);
         }
     }
 
     /**
      * 获取登录用户名
+     *
      * @return
      */
-    public static String getUserName(){
+    public static String getUserName() {
 
         Map<String, String> map = current.get();
-        if (map == null){
+        if (map == null) {
             return null;
         }
         return map.get("userName");
